cb19eabdd097e83bc663b47dea447e8b0e1cb501
relatoriocaixas <recebedoriamovebuss@outlook.com>
2025-10-27 13:27:29 -0300

    Add files via upload

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,8 +1,32 @@
+// Firebase init (v10 modular)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
+import { 
+  getAuth, 
+  setPersistence, 
+  browserLocalPersistence, 
+  onAuthStateChanged, 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword, 
+  signOut, 
+  updatePassword 
+} from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+import { 
+  getFirestore, 
+  doc, 
+  setDoc, 
+  getDoc, 
+  updateDoc, 
+  addDoc, 
+  getDocs, 
+  collection, 
+  query, 
+  where, 
+  serverTimestamp, 
+  orderBy 
+} from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 
-const firebaseConfig = {
+// Config do Firebase unificado
+export const firebaseConfig = {
   apiKey: "AIzaSyBWmq02P8pGbl2NmppEAIKtF9KtQ7AzTFQ",
   authDomain: "unificado-441cd.firebaseapp.com",
   projectId: "unificado-441cd",
@@ -12,6 +36,30 @@ const firebaseConfig = {
   measurementId: "G-6GQX395J9C"
 };
 
+// Inicializa app, auth e db
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+// Mantém persistência local como no backup antigo
+setPersistence(auth, browserLocalPersistence);
+
+// Exporta todas as funções usadas no app.js
+export { 
+  onAuthStateChanged, 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword, 
+  signOut, 
+  updatePassword, 
+  doc, 
+  setDoc, 
+  getDoc, 
+  updateDoc, 
+  addDoc, 
+  getDocs, 
+  collection, 
+  query, 
+  where, 
+  serverTimestamp, 
+  orderBy 
+};
